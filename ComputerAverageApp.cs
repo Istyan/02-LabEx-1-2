52c88b25459138dafae3d3d4804bac82515ade74
Istyan <christianlat03@gmail.com>
2024-08-20 16:40:32 +0800

    Add files via upload

---
+++ ComputerAverageApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ComputerAverageProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Allowing the user to input
+            Console.WriteLine("Enter five(5) grades seperated by new line: ");
+            double gradescr1 = Convert.ToDouble(Console.ReadLine());
+            double gradescr2 = Convert.ToDouble(Console.ReadLine());
+            double gradescr3 = Convert.ToDouble(Console.ReadLine());
+            double gradescr4 = Convert.ToDouble(Console.ReadLine());
+            double gradescr5 = Convert.ToDouble(Console.ReadLine());
+
+            //To get the average grade 
+            double sum = gradescr1 + gradescr2 + gradescr3 + gradescr4 + gradescr5;
+            double averageScore = sum / 5;
+            double averageRounded = Math.Round(averageScore); //to get the round of the average
+
+            Console.WriteLine("The Average Grade is: " + averageScore);
+            Console.WriteLine("The Round-off Average Grade is: " + (averageRounded));
+            Console.WriteLine("Thank you!");
+
+        }
+    }
+}
---
+++ DataTypesApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataTypesProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Allowing the user to input
+            Console.Write("Enter the pieces of apple: ");
+            int pcs = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the total price of the apple(s): ");
+            double price = Convert.ToDouble(Console.ReadLine());    
+            double wholenum = Math.Round(price, 1); 
+
+            // To print the total and the values of the original and converted prices
+            Console.WriteLine("The total price of " + pcs + " apple(s) is " + wholenum);
+            Console.WriteLine("Value of Original Price: " + price);
+
+            int finalvalue = (int)wholenum;
+            Console.WriteLine("Value of Converted Price: "+finalvalue);
+            Console.WriteLine("Thank You!");
+        }
+    }
+}
